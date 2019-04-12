d35e2bd6bea2dd0f05bb106de1aa020187638673
Antavio <njuguna13@gmail.com>
2019-04-12 11:52:50 +0300

    Create a news source view function with a dynamic id as the argument.

--- app/views.py
+++ app/views.py
@@ -6,4 +6,12 @@ def index():
     '''
     Function that returns the index page & its data
     '''
-    return render_template('index.html')
+    message = "Welcome to NewsHighlight"
+    return render_template('index.html', messages = message)
+
+@app.route('/news_source/<int:news_id>')
+def news_source(news_id):
+    '''
+    News source page function that will return news sources plus data
+    '''
+    return render_template('news_source.html',id = news_id)
