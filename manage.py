35d3795a52019ad962394a8baa1bc1760ca09c5e
Antavio <njuguna13@gmail.com>
2019-04-15 10:26:55 +0300

    Create a Procfile file which enables running of the flask application using gunicorn.

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def test():
     Run all the unittests
     '''
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
